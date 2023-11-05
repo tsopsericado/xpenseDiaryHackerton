068e4b7298328b67b7e912d69a0073631812dbcb
ricardo <ricardotsopse@gmail.com>
2023-11-05 10:05:29 +0100

    development branch created

---
+++ app/atom/button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="bg-green-400 border border-black px-4 py-2">
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
-import DeployButton from '../components/DeployButton'
-import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
-import Header from '@/components/Header'
-import { cookies } from 'next/headers'
+import DeployButton from "../components/DeployButton";
+import AuthButton from "../components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
+import SignUpUserSteps from "@/components/SignUpUserSteps";
+import Header from "@/components/Header";
+import { cookies } from "next/headers";
+import Button from "./atom/button";
 
 export default async function Index() {
-  const cookieStore = cookies()
+  const cookieStore = cookies();
 
   const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
     try {
-      createClient(cookieStore)
-      return true
+      createClient(cookieStore);
+      return true;
     } catch (e) {
-      return false
+      return false;
     }
-  }
+  };
 
-  const isSupabaseConnected = canInitSupabaseClient()
+  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -29,6 +30,7 @@ export default async function Index() {
           <DeployButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
+        <Button />
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
@@ -41,7 +43,7 @@ export default async function Index() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{' '}
+          Powered by{" "}
           <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
@@ -53,5 +55,5 @@ export default async function Index() {
         </p>
       </footer>
     </div>
-  )
+  );
 }
